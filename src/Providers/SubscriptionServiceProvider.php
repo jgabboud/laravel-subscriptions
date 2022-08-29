79c64804d6d3655d0037e0e0ab899657e913fee6
= <=>
2022-08-29 20:51:05 +0300

    updating package

--- src/Providers/SubscriptionServiceProvider.php
+++ src/Providers/SubscriptionServiceProvider.php
@@ -24,5 +24,6 @@ class SubscriptionServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'../Database/migrations');
     }
 }
