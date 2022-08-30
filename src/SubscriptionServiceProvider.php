ec7312224a1835ae4417d8264f1ac7815690942c
= <=>
2022-08-30 13:36:41 +0300

    testing with migrations publishing

--- src/SubscriptionServiceProvider.php
+++ src/SubscriptionServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Jgabboud\Subscriptions;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
 
 class SubscriptionServiceProvider extends ServiceProvider
 {
@@ -24,16 +25,38 @@ class SubscriptionServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
+        // $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
         
-        if ($this->app->runningInConsole()) {
-            // Export the migration
-            if (! class_exists('CreatePlansTable')) {
-              $this->publishes([
-                __DIR__ . '/../database/migrations/create_plans_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_plans_table.php'),
-                // you can add any number of migrations here
-              ], 'migrations');
-            }
-          }
+        // if ($this->app->runningInConsole()) {
+        //     // Export the migration
+
+        //     if (! class_exists('CreatePlansTable')) {
+              
+        //     }
+
+        //   }
+
+        $this->publishes([
+          __DIR__ . '/../database/migrations/create_plans_table.php.stub' => $this->getMigrationFileName('create_plans_tables.php'),
+        ], 'migrations');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @return string
+     */
+    protected function getMigrationFileName($migrationFileName)
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem, $migrationFileName) {
+                return $filesystem->glob($path.'*_'.$migrationFileName);
+            })
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
