b380796230dcb56328fb863ad5ac9e1d2e5207ec
= <=>
2022-09-02 10:05:42 +0300

    updating has subscriptions

--- src/Traits/HasSubscriptions.php
+++ src/Traits/HasSubscriptions.php
@@ -2,13 +2,17 @@
 
 namespace Jgabboud\Subscriptions\Traits;
 
+use Carbon\Carbon;
+use Jgabboud\Subscriptions\Models\Plan;
 use Jgabboud\Subscriptions\Models\PlanSubscription;
 
 trait HasSubscriptions
 {
     abstract public function morphMany($related, $name, $type = null, $id = null, $localKey = null);
 
-    //
+// == DECLARATION
+
+    //-- default delete
     protected static function bootHasSubscriptions()
     {
         static::deleted(function ($plan) {
@@ -16,52 +20,56 @@ trait HasSubscriptions
         });
     }
 
-    //
+    //-- RELATION 
     public function planSubscriptions()
     {
         return $this->morphMany(PlanSubscription::class, 'subscriber', 'subscriber_type', 'subscriber_id');
     }
+//
 
-    //
+// == QUERIES
+
+    //-- get currently active subscriptions
     public function activePlanSubscriptions()
     {
         return $this->planSubscriptions->reject->inactive();
     }
 
-    //
+    //-- get subscriptions by slug
     public function planSubscription(string $subscriptionSlug): ?PlanSubscription
     {
         return $this->planSubscriptions()->where('slug', $subscriptionSlug)->first();
     }
 
-    //
+    //-- get plans that are currently subscribed to
     public function subscribedPlans()
     {
-        // $planIds = $this->planSubscriptions->reject->inactive()->pluck('plan_id')->unique();
-
-        // return app('plans')->whereIn('id', $planIds)->get();
+        $planIds = $this->planSubscriptions->reject->inactive()->pluck('plan_id')->unique();
+        return Plan::whereIn('id', $planIds)->get();
     }
 
-    //
+    //-- check if user is subscribed to this plan 
     public function subscribedTo($planId): bool
     {
         $subscription = $this->planSubscriptions()->where('plan_id', $planId)->first();
-
         return $subscription && $subscription->active();
     }
 
-    //
-    // public function newPlanSubscription($subscription, Plan $plan, Carbon $startDate = null): PlanSubscription
-    // {
-    //     $trial = new Period($plan->trial_interval, $plan->trial_period, $startDate ?? now());
-    //     $period = new Period($plan->invoice_interval, $plan->invoice_period, $trial->getEndDate());
+    //-- subscribe to a new plan
+    public function subscribe($subscription, Plan $plan, Carbon $startDate = null)#: PlanSubscription
+    {
+        // $trial = new Period($plan->trial_interval, $plan->trial_period, $startDate ?? now());
+        // $period = new Period($plan->invoice_interval, $plan->invoice_period, $trial->getEndDate());
 
-    //     return $this->planSubscriptions()->create([
-    //         'name' => $subscription,
-    //         'plan_id' => $plan->getKey(),
-    //         'trial_ends_at' => $trial->getEndDate(),
-    //         'starts_at' => $period->getStartDate(),
-    //         'ends_at' => $period->getEndDate(),
-    //     ]);
-    // }
+        // return $this->planSubscriptions()->create([
+        //     'name' => $subscription,
+        //     'plan_id' => $plan->getKey(),
+        //     'trial_ends_at' => $trial->getEndDate(),
+        //     'starts_at' => $period->getStartDate(),
+        //     'ends_at' => $period->getEndDate(),
+        // ]);
+    }
+
+//
+
 }
